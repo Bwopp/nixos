6fb45fddf84e81434a2d82e2c3fba4a41790baba
bwop <bwopp@proton.me>
2026-03-05 22:38:56 +1300

    2026-03-05 22:38:56

--- home-manager/floorp.nix
+++ home-manager/floorp.nix
@@ -54,35 +54,6 @@ in
         extraPolicies = {
           DisableTelemetry = true;
           ExtensionSettings = builtins.listToAttrs extensions;
-          SearchEngines = {
-            Default = "ddg";
-            Add = [
-              {
-                Name = "nixpkgs packages";
-                URLTemplate = "https://search.nixos.org/packages?query={searchTerms}";
-                IconURL = "https://wiki.nixos.org/favicon.ico";
-                Alias = "@np";
-              }
-              {
-                Name = "NixOS options";
-                URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
-                IconURL = "https://wiki.nixos.org/favicon.ico";
-                Alias = "@no";
-              }
-              {
-                Name = "NixOS Wiki";
-                URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
-                IconURL = "https://wiki.nixos.org/favicon.ico";
-                Alias = "@nw";
-              }
-              {
-                Name = "noogle";
-                URLTemplate = "https://noogle.dev/q?term={searchTerms}";
-                IconURL = "https://noogle.dev/favicon.ico";
-                Alias = "@ng";
-              }
-            ];
-          };
         };
       }
     )
