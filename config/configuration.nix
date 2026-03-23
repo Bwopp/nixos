6374c768531cf8c05f01cdb4fe7ddc8d68f693bc
bwop <bwopp@proton.me>
2026-03-23 20:26:11 +1300

    2026-03-23 20:26:11

--- config/configuration.nix
+++ config/configuration.nix
@@ -56,7 +56,6 @@
     nameservers = [ "1.1.1.1" "1.0.0.1" ];
     networkmanager = { 
       wifi.backend = "wpa_supplicant";
-      # dns = "none";
       enable = true;
       plugins = with pkgs; [
         networkmanager-openvpn
@@ -158,6 +157,30 @@
   # Shell
   programs.fish = {
     enable = true;
+      interactiveShellInit = ''
+        # TokyoNight Storm
+        set -g fish_color_normal c0caf5
+        set -g fish_color_command 7dcfff
+        set -g fish_color_keyword bb9af7
+        set -g fish_color_quote e0af68
+        set -g fish_color_redirection c0caf5
+        set -g fish_color_end ff9e64
+        set -g fish_color_option bb9af7
+        set -g fish_color_error f7768e
+        set -g fish_color_param 9d7cd8
+        set -g fish_color_comment 565f89
+        set -g fish_color_selection --background=2e3c64
+        set -g fish_color_search_match --background=2e3c64
+        set -g fish_color_operator 9ece6a
+        set -g fish_color_escape bb9af7
+        set -g fish_color_autosuggestion 565f89
+        set -g fish_pager_color_progress 565f89
+        set -g fish_pager_color_prefix 7dcfff
+        set -g fish_pager_color_completion c0caf5
+        set -g fish_pager_color_description 565f89
+        set -g fish_pager_color_selected_background --background=2e3c64
+      '';
+
     shellAliases = {
       ll = "ls -l";
       tup = "tailscale up";
