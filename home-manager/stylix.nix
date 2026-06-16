76649c2d878a2ea635781ace2bb5b119abc94909
bwop <bwopp@proton.me>
2026-06-16 20:51:54 +1200

    2026-06-16 20:51:54

--- home-manager/stylix.nix
+++ home-manager/stylix.nix
@@ -3,5 +3,10 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    cursor = {
+      name = "breeze_cursors";
+      package = pkgs.kdePackages.breeze;
+      size = 20;
+    };
   };
 }
