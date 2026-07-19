096fe6b05bc3b2c95ec9281298e37669f02dc3e4
bwop <bwopp@proton.me>
2026-07-19 23:26:00 +1200

    2026-07-19 23:26:00

--- modules/fish.nix
+++ modules/fish.nix
@@ -12,6 +12,8 @@
       nixgitpush = "git -C /etc/nixos add . && git -C /etc/nixos commit -m (date '+%Y-%m-%d %H:%M:%S') && git -C /etc/nixos push";
       wup = "warp-cli connect";
       wdown = "warp-cli disconnect";
+      xup = "sudo systemctl start xray-client";
+      xdown = "sudo systemctl stop xray-client";
     };
   };
 
