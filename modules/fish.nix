fcba07beed7d0db371a6956cf0aeffa9dc2f0322
bwop <bwop@bwop.org>
2026-08-06 15:51:32 +1200

    2026-08-06 15:51:32

--- modules/fish.nix
+++ modules/fish.nix
@@ -9,7 +9,11 @@
       rebuild = "sudo nixos-rebuild switch";
       update = "sudo nix flake update --flake /etc/nixos/";
       gc = "sudo nix-collect-garbage";
+      optimise = "sudo nix-store --optimise";
+      deleteold = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +5";
+      fullclean = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +5 && sudo nix-collect-garbage && sudo nix-store --optimise";
       nixgitpush = "git -C /etc/nixos add . && git -C /etc/nixos commit -m (date '+%Y-%m-%d %H:%M:%S') && git -C /etc/nixos push";
+      nixgitpull = "git -C /etc/nixos pull";
       wup = "warp-cli connect";
       wdown = "warp-cli disconnect";
       vup = "sudo systemctl start sing-box";
