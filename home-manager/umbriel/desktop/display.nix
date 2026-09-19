d534a3acc44e3ae5e0fa858262540b6d70dc1063
bwop <bwop@bwop.org>
2026-09-19 12:27:42 +1200

    2026-09-19 12:27:42

--- home-manager/umbriel/desktop/display.nix
+++ home-manager/umbriel/desktop/display.nix
@@ -1,12 +1,12 @@
 {
   output = {
-    "DP-3" = {
+    "LG Electronics 27GL850 010NTJJ8R044" = {
       mode = "2560x1440@144.0";
       scale = 1.0;
       position = [ 0 0 ];
       tearing = true;
     };
-    "DP-4" = {
+    "Lenovo Group Limited T24i-10 VT685808" = {
       mode = "1920x1080@60.0";
       scale = 1.0;
       position = [ 2560 0 ];
--- home-manager/umbriel/laptop/display.nix
+++ home-manager/umbriel/laptop/display.nix
@@ -1,6 +1,6 @@
 {
   output = {
-    "eDP-1" = {
+    "EDO 0x4145 Unknown" = {
       mode = "3000x1876@120.001";
       scale = 1.7166666666666666;
       position = [ 0 0 ];
